1909aa705d18f1384690f9e1ec4e0fde80530edd
Dylan Harness <dharness@Dylans-MacBook-Pro.local>
2015-01-20 14:06:24 -0500

    ..

--- Ryan_Web/public/app.js
+++ Ryan_Web/public/app.js
@@ -1,16 +1,16 @@
  var myapp = angular.module('myapp', ['ngRoute']);
 
- myapp.config(function($routeProvider) {
-$routeProvider
-// home page
-.when('/', {
-templateUrl: 'pages/landing.html',
-controller: 'mainController'
-})
+ myapp.config(function() {
 
-//signup page
-.when('/blog', {
-templateUrl: 'pages/blog.html',
-controller: 'blogController'
-});
+	// home page
+	.when('/', {
+		templateUrl: 'pages/landing.html',
+		controller: 'mainController'
+	})
+
+	//signup page
+	.when('/blog', {
+		templateUrl: 'pages/blog.html',
+		controller: 'blogController'
+	});
 });
